f4d1d81457134a7c58525bc7728d48ab2c619103
Jafrin Akter Alo <81787520+JafrinAlo@users.noreply.github.com>
2022-07-31 23:59:51 +0600

    Update script.js

--- Florence/script.js
+++ Florence/script.js
@@ -26,6 +26,6 @@ video.addEventListener('play', () => {
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+   // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
